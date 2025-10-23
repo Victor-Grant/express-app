e41ec59d88a8305a7f6bff0a31672239cc99e7b3
Victor Grant <grantvictor725@gmail.com>
2025-10-23 04:08:34 -0700

    .env setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require("express")();
 const apiRoutes = require("./routes/api");
+require("dotenv").config();
 
 app.use("/api/users", apiRoutes);
 const PORT = process.env.PORT || 3000;
